f3851433953432a9e161c31e7287250790d2e12a
Jeremy Pritts <jrpritts7@gmail.com>
2021-09-21 21:56:29 -0400

    deep clone unity node

--- AssemblyDumper/Unity/UnityNode.cs
+++ AssemblyDumper/Unity/UnityNode.cs
@@ -13,5 +13,25 @@ namespace AssemblyDumper.Unity
 		public byte TypeFlags { get; set; }
 		public int MetaFlag { get; set; }
 		public List<UnityNode> SubNodes { get; set; }
+
+		/// <summary>
+		/// Deep clones a node and all its subnodes<br/>
+		/// Warning: Deep cloning a node with a circular hierarchy will cause an endless loop
+		/// </summary>
+		/// <returns>The new node</returns>
+		public UnityNode DeapClone()
+		{
+			var cloned = new UnityNode();
+			cloned.TypeName = new string(TypeName);
+			cloned.Name = new string(Name);
+			cloned.Level = Level;
+			cloned.ByteSize = ByteSize;
+			cloned.Index = Index;
+			cloned.Version = Version;
+			cloned.TypeFlags = TypeFlags;
+			cloned.MetaFlag = MetaFlag;
+			cloned.SubNodes = SubNodes.ConvertAll(x => x.DeapClone());
+			return cloned;
+		}
 	}
 }
