2e9af60cea5bf6f22ccda0acde5a5c6c0135d77c
Jeremy Pritts <jrpritts7@gmail.com>
2021-12-01 23:34:48 -0500

    use empty arrays in the dependency methods for now

--- AssemblyDumper/Passes/Pass54_FillDependencyMethods.cs
+++ AssemblyDumper/Passes/Pass54_FillDependencyMethods.cs
@@ -1,9 +1,8 @@
 ﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AssemblyDumper.Passes
 {
@@ -12,9 +11,20 @@ namespace AssemblyDumper.Passes
 		public static void DoPass()
 		{
 			Console.WriteLine("Pass 54: Fill Fetch Dependency Methods");
-			foreach(TypeDefinition type in SharedState.TypeDictionary.Values)
+
+			TypeReference commonPPtrTypeRef = SharedState.Module.ImportCommonType("AssetRipper.Core.Classes.Misc.PPtr`1");
+			TypeReference unityObjectBaseInterfaceRef = SharedState.Module.ImportCommonType<AssetRipper.Core.Interfaces.IUnityObjectBase>();
+			GenericInstanceType unityObjectBasePPtrRef = commonPPtrTypeRef.MakeGenericInstanceType(unityObjectBaseInterfaceRef);
+			MethodReference emptyArray = SharedState.Module.ImportSystemMethod<System.Array>(method => method.Name == "Empty");
+			var emptyArrayMethod = new GenericInstanceMethod(emptyArray);
+			emptyArrayMethod.GenericArguments.Add(unityObjectBasePPtrRef);
+
+			foreach (TypeDefinition type in SharedState.TypeDictionary.Values)
 			{
-				type.GetDependencyMethod().Body.GetILProcessor().EmitNotSupportedException();
+				var method = type.GetDependencyMethod();
+				ILProcessor processor = method.Body.GetILProcessor();
+				processor.Emit(OpCodes.Call, emptyArrayMethod);
+				processor.Emit(OpCodes.Ret);
 			}
 		}
 
