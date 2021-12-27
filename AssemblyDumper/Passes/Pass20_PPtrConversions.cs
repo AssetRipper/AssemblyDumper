96c183bf272a9388bb04d3a7429a2674d4136aa1
Jeremy Pritts <jrpritts7@gmail.com>
2021-12-27 14:20:34 -0500

    add pptr explicit conversions for gameobject and component

--- AssemblyDumper/Passes/Pass20_PPtrConversions.cs
+++ AssemblyDumper/Passes/Pass20_PPtrConversions.cs
@@ -10,66 +10,67 @@ namespace AssemblyDumper.Passes
 {
 	public static class Pass20_PPtrConversions
 	{
-		private static ITypeDefOrRef commonPPtrType;
-		private static ITypeDefOrRef unityObjectBaseInterface;
-		private static GenericInstanceTypeSignature unityObjectBasePPtr;
 		const MethodAttributes ConversionAttributes = MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.SpecialName | MethodAttributes.HideBySig;
 
 		public static void DoPass()
 		{
 			System.Console.WriteLine("Pass 20: PPtr Conversions");
 
-			commonPPtrType = SharedState.Importer.ImportCommonType("AssetRipper.Core.Classes.Misc.PPtr`1");
-			unityObjectBaseInterface = SharedState.Importer.ImportCommonType<IUnityObjectBase>();
-			unityObjectBasePPtr = commonPPtrType.MakeGenericInstanceType(unityObjectBaseInterface.ToTypeSignature());
+			ITypeDefOrRef commonPPtrType = SharedState.Importer.ImportCommonType("AssetRipper.Core.Classes.Misc.PPtr`1");
 
 			foreach (string name in SharedState.ClassDictionary.Keys)
 			{
 				if (name.StartsWith("PPtr_"))
 				{
+					TypeDefinition pptrType = SharedState.TypeDictionary[name];
+
 					string parameterTypeName = name.Substring(5, name.LastIndexOf('_') - 5);
 					TypeDefinition parameterType = SharedState.TypeDictionary[parameterTypeName];
-					TypeDefinition pptrType = SharedState.TypeDictionary[name];
-					AddImplicitConversion(pptrType, parameterType);
-					AddExplicitConversion(pptrType);
+					GenericInstanceTypeSignature implicitConversionResultType = commonPPtrType.MakeGenericInstanceType(parameterType.ToTypeSignature());
+
+					pptrType.AddImplicitConversion(implicitConversionResultType);
+					pptrType.AddExplicitConversion<IUnityObjectBase>(commonPPtrType);
+					if (name == "PPtr_GameObject_")
+					{
+						pptrType.AddExplicitConversion<AssetRipper.Core.Classes.GameObject.IGameObject>(commonPPtrType);
+					}
+					else if (name == "PPtr_Component_")
+					{
+						pptrType.AddExplicitConversion<AssetRipper.Core.Classes.IComponent>(commonPPtrType);
+					}
 				}
 			}
 		}
 
-		private static void AddImplicitConversion(TypeDefinition pptrType, TypeDefinition parameterType)
+		private static MethodDefinition AddImplicitConversion(this TypeDefinition pptrType, GenericInstanceTypeSignature resultTypeSignature)
 		{
-			GenericInstanceTypeSignature conversionResultType = commonPPtrType.MakeGenericInstanceType(parameterType.ToTypeSignature());
-			MemberReference constructor = MethodUtils.MakeConstructorOnGenericType(conversionResultType, 2);
-
-			FieldDefinition fileID = pptrType.Fields.Single(field => field.Name == "m_FileID");
-			FieldDefinition pathID = pptrType.Fields.Single(f => f.Name == "m_PathID");
-
-			MethodDefinition implicitMethod = pptrType.AddMethod("op_Implicit", ConversionAttributes, conversionResultType);
-			implicitMethod.AddParameter("value", pptrType);
-
-			implicitMethod.CilMethodBody.InitializeLocals = true;
-			var processor = implicitMethod.CilMethodBody.Instructions;
-
-			processor.Add(CilOpCodes.Ldarg_0);
-			processor.Add(CilOpCodes.Ldfld, fileID);
-			processor.Add(CilOpCodes.Ldarg_0);
-			processor.Add(CilOpCodes.Ldfld, pathID);
-			processor.Add(CilOpCodes.Newobj, constructor);
-			processor.Add(CilOpCodes.Ret);
+			return pptrType.AddConversion(resultTypeSignature, false);
 		}
 
-		private static void AddExplicitConversion(TypeDefinition pptrType)
+		private static MethodDefinition AddExplicitConversion(this TypeDefinition pptrType, GenericInstanceTypeSignature resultTypeSignature)
 		{
-			MemberReference constructor = MethodUtils.MakeConstructorOnGenericType(unityObjectBasePPtr, 2);
+			return pptrType.AddConversion(resultTypeSignature, true);
+		}
+
+		private static MethodDefinition AddExplicitConversion<T>(this TypeDefinition pptrType, ITypeDefOrRef commonPPtrType)
+		{
+			ITypeDefOrRef importedInterface = SharedState.Importer.ImportCommonType<T>();
+			GenericInstanceTypeSignature resultPPtrSignature = commonPPtrType.MakeGenericInstanceType(importedInterface.ToTypeSignature());
+			return pptrType.AddExplicitConversion(resultPPtrSignature);
+		}
+
+		private static MethodDefinition AddConversion(this TypeDefinition pptrType, GenericInstanceTypeSignature resultTypeSignature, bool isExplicit)
+		{
+			MemberReference constructor = MethodUtils.MakeConstructorOnGenericType(resultTypeSignature, 2);
 
 			FieldDefinition fileID = pptrType.Fields.Single(field => field.Name == "m_FileID");
 			FieldDefinition pathID = pptrType.Fields.Single(f => f.Name == "m_PathID");
 
-			MethodDefinition explicitMethod = pptrType.AddMethod("op_Explicit", ConversionAttributes, unityObjectBasePPtr);
-			explicitMethod.AddParameter("value", pptrType);
+			string methodName = isExplicit ? "op_Explicit" : "op_Implicit";
+			MethodDefinition method = pptrType.AddMethod(methodName, ConversionAttributes, resultTypeSignature);
+			method.AddParameter("value", pptrType);
 
-			explicitMethod.CilMethodBody.InitializeLocals = true;
-			var processor = explicitMethod.CilMethodBody.Instructions;
+			var processor = method.CilMethodBody.Instructions;
 
 			processor.Add(CilOpCodes.Ldarg_0);
 			processor.Add(CilOpCodes.Ldfld, fileID);
@@ -77,6 +78,8 @@ namespace AssemblyDumper.Passes
 			processor.Add(CilOpCodes.Ldfld, pathID);
 			processor.Add(CilOpCodes.Newobj, constructor);
 			processor.Add(CilOpCodes.Ret);
+
+			return method;
 		}
 	}
 }
