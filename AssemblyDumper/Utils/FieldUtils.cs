ecb5ba4b3f2ef4dec9b11f90f7d965b069b966af
Jeremy Pritts <jrpritts7@gmail.com>
2022-01-04 18:18:28 -0500

    allow getting fields in a base type definition

--- AssemblyDumper/Utils/FieldUtils.cs
+++ AssemblyDumper/Utils/FieldUtils.cs
@@ -18,18 +18,25 @@ namespace AssemblyDumper.Utils
 
 		public static FieldDefinition GetFieldByName(this TypeDefinition type, string fieldName)
 		{
-			return type.Fields.Single(field => field.Name == fieldName);
+			return type.TryGetFieldByName(fieldName)
+				?? throw new Exception($"{type.FullName} doesn't have a {fieldName} field");
 		}
 
 		public static bool TryGetFieldByName(this TypeDefinition type, string fieldName, [NotNullWhen(true)][MaybeNullWhen(false)] out FieldDefinition? field)
 		{
-			field = type.Fields.SingleOrDefault(field => field.Name == fieldName);
+			field = type.TryGetFieldByName(fieldName);
 			return field != null;
 		}
 
 		public static FieldDefinition? TryGetFieldByName(this TypeDefinition type, string fieldName)
 		{
-			return type.Fields.SingleOrDefault(field => field.Name == fieldName);
+			return type.Fields.SingleOrDefault(field => field.Name == fieldName)
+				?? type.TryGetBaseTypeDefinition()?.TryGetFieldByName(fieldName);
+		}
+
+		private static TypeDefinition? TryGetBaseTypeDefinition(this TypeDefinition type)
+		{
+			return type.BaseType as TypeDefinition;
 		}
 	}
 }
