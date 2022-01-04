b7a37fecd785bbbb664599a0dadf24dab7246008
Jeremy Pritts <jrpritts7@gmail.com>
2022-01-03 20:52:51 -0500

    use the command argument

--- generate_all.sh
+++ generate_all.sh
@@ -17,7 +17,13 @@ generate() {
     $path_to_AssemblyDumper --output $path_to_OutputFolder --runtime $path_to_RuntimeLibrary --collections $path_to_CollectionsLibrary $path_to_JsonFolder/$j
 }
 
-vers=($(./get_names.sh))
+if [ $# = 0 ]
+then
+    vers=($(./get_names.sh))
+else
+    vers=($(./get_names.sh $1))
+fi
+
 echo Generating assemblies for ${#vers[@]} Unity versions
 for ((i=0; i<${#vers[@]}; i++)); 
 do
