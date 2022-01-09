261fe1f74f15a9e27852aa29b242316cc0740dff
Jeremy Pritts <jrpritts7@gmail.com>
2022-01-09 03:32:06 -0500

    font interface

--- AssemblyDumper/Passes/Pass62_MiscellaneousExporters.cs
+++ AssemblyDumper/Passes/Pass62_MiscellaneousExporters.cs
@@ -1,5 +1,6 @@
 ﻿using AssemblyDumper.Utils;
 using AssetRipper.Core.Classes;
+using AssetRipper.Core.Classes.Font;
 
 namespace AssemblyDumper.Passes
 {
@@ -10,12 +11,19 @@ namespace AssemblyDumper.Passes
 			Console.WriteLine("Pass 62: Miscellaneous Exporters");
 
 			SharedState.TypeDictionary["TextAsset"].ImplementTextAsset();
+			SharedState.TypeDictionary["Font"].ImplementFontAsset();
 		}
 
 		private static void ImplementTextAsset(this TypeDefinition type)
 		{
 			type.AddInterfaceImplementation<ITextAsset>();
 			type.ImplementGetterProperty(nameof(ITextAsset.Script), InterfaceUtils.InterfacePropertyImplementation, SystemTypeGetter.String, type.GetFieldByName("m_Script"));
+		}
+
+		private static void ImplementFontAsset(this TypeDefinition type)
+		{
+			type.AddInterfaceImplementation<IFont>();
+			type.ImplementFullProperty(nameof(IFont.FontData), InterfaceUtils.InterfacePropertyImplementation, SystemTypeGetter.UInt8.MakeSzArrayType(), type.GetFieldByName("m_FontData"));
 		}
 	}
 }
