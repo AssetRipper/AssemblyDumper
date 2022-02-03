4142ed77706c8a10dfb4d28dc5278cc61b8bf1e6
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-03 13:46:56 -0500

    remove hash boxing

--- AssemblyDumper/Passes/Pass72_FixGuidAndHashYaml.cs
+++ AssemblyDumper/Passes/Pass72_FixGuidAndHashYaml.cs
@@ -11,19 +11,19 @@ namespace AssemblyDumper.Passes
 			{
 				MethodDefinition releaseMethod = guidType.Methods.Single(m => m.Name == "ExportYAMLRelease");
 				MethodDefinition editorMethod = guidType.Methods.Single(m => m.Name == "ExportYAMLEditor");
-				releaseMethod.FixYaml<UnityGUID>();
-				editorMethod.FixYaml<UnityGUID>();
+				releaseMethod.FixGuidYaml<UnityGUID>();
+				editorMethod.FixGuidYaml<UnityGUID>();
 			}
 			if (SharedState.TypeDictionary.TryGetValue("Hash128", out TypeDefinition? hashType))
 			{
 				MethodDefinition releaseMethod = hashType.Methods.Single(m => m.Name == "ExportYAMLRelease");
 				MethodDefinition editorMethod = hashType.Methods.Single(m => m.Name == "ExportYAMLEditor");
-				releaseMethod.FixYaml<Hash128>();
-				editorMethod.FixYaml<Hash128>();
+				releaseMethod.FixHashYaml<Hash128>();
+				editorMethod.FixHashYaml<Hash128>();
 			}
 		}
 
-		private static void FixYaml<T>(this MethodDefinition method)
+		private static void FixGuidYaml<T>(this MethodDefinition method)
 		{
 			MethodDefinition conversionMethod = method.DeclaringType!.Methods.Single(m => m.Name == "op_Implicit");
 			ITypeDefOrRef commonRef = SharedState.Importer.ImportCommonType<T>();
@@ -38,6 +38,24 @@ namespace AssemblyDumper.Passes
 			processor.Add(CilOpCodes.Call, conversionMethod);
 			processor.Add(CilOpCodes.Stloc, local);
 			processor.Add(CilOpCodes.Ldloca, local);
+			processor.Add(CilOpCodes.Ldarg_1);
+			processor.Add(CilOpCodes.Call, exportMethod);
+			processor.Add(CilOpCodes.Ret);
+			processor.OptimizeMacros();
+		}
+
+		private static void FixHashYaml<T>(this MethodDefinition method)
+		{
+			MethodDefinition conversionMethod = method.DeclaringType!.Methods.Single(m => m.Name == "op_Implicit");
+			ITypeDefOrRef commonRef = SharedState.Importer.ImportCommonType<T>();
+			IMethodDefOrRef exportMethod = SharedState.Importer.ImportCommonMethod<T>(m => m.Name == "ExportYAML");
+			method.CilMethodBody!.LocalVariables.Clear();
+			CilLocalVariable local = new CilLocalVariable(commonRef.ToTypeSignature());
+			method.CilMethodBody.LocalVariables.Add(local);
+			CilInstructionCollection processor = method.CilMethodBody.Instructions;
+			processor.Clear();
+			processor.Add(CilOpCodes.Ldarg_0);
+			processor.Add(CilOpCodes.Call, conversionMethod);
 			processor.Add(CilOpCodes.Ldarg_1);
 			processor.Add(CilOpCodes.Call, exportMethod);
 			processor.Add(CilOpCodes.Ret);
