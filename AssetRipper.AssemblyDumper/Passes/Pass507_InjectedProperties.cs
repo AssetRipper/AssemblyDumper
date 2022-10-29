dbf8f1dd4ced102cb1d7268294c5e593df79e206
Jeremy Pritts <jrpritts7@gmail.com>
2022-10-29 18:51:37 -0400

    Remove injected property on Texture2D

--- AssetRipper.AssemblyDumper/Passes/Pass507_InjectedProperties.cs
+++ AssetRipper.AssemblyDumper/Passes/Pass507_InjectedProperties.cs
@@ -10,7 +10,6 @@ namespace AssetRipper.AssemblyDumper.Passes
 		public static void DoPass()
 		{
 			SceneAssetTargetScene();
-			TextureTerrainData();
 		}
 
 		private static void SceneAssetTargetScene()
@@ -18,13 +17,6 @@ namespace AssetRipper.AssemblyDumper.Passes
 			ClassGroup group = SharedState.Instance.ClassGroups[1032]; //SceneAsset
 			TypeSignature propertyType = SharedState.Instance.Importer.ImportType<AssetCollection>().ToTypeSignature();
 			PropertyInjector.InjectFullProperty(group, propertyType, TargetSceneName, true);
-		}
-
-		private static void TextureTerrainData()
-		{
-			ClassGroup group = SharedState.Instance.ClassGroups[28]; //Texture2D
-			TypeSignature propertyType = SharedState.Instance.ClassGroups[156].Interface.ToTypeSignature();//ITerrainData
-			PropertyInjector.InjectFullProperty(group, propertyType, TerrainDataName, true);
 		}
 	}
 }
