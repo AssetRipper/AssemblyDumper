155833d5c28a6dfb91948c301c40d3947be6233c
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-10-10 15:36:06 -0400

    Improve interface inheritance

--- AssetRipper.AssemblyDumper/Passes/Pass920_InterfaceInheritance.cs
+++ AssetRipper.AssemblyDumper/Passes/Pass920_InterfaceInheritance.cs
@@ -6,61 +6,118 @@ namespace AssetRipper.AssemblyDumper.Passes
 	{
 		public static void DoPass()
 		{
-			foreach (ClassGroup group in SharedState.Instance.ClassGroups.Values)
+			foreach (ClassGroup group in SharedState.Instance.ClassGroups.Values
+				.OrderBy(g => g.Types.Select(t => t.GetInheritanceDepth()).Max())
+				.Order(GroupComparer.Instance))
 			{
 				DoPassOnGroup(group);
 			}
 		}
 
-		private static void DoPassOnGroup(this ClassGroup group)
+		private static void DoPassOnGroup(ClassGroup group)
 		{
-			if (TryGetBaseTypeDefinitions(group.Types, out List<TypeDefinition>? baseTypes))
+			List<HashSet<ITypeDefOrRef>> instances = group.Types.Select(t => t.GetAllInterfaces()).ToList();
+			HashSet<ITypeDefOrRef> existingInterfaces = group.Interface.GetAllInterfaces();
+			existingInterfaces.Add(group.Interface);
+
+			foreach (ITypeDefOrRef potentialInterface in instances.First())
 			{
-				List<ITypeDefOrRef> interfaces = baseTypes.First().GetAllInterfaces().ToList();
-				foreach (ITypeDefOrRef @interface in interfaces)
+				if (!existingInterfaces.Contains(potentialInterface) && instances.All(i => i.Contains(potentialInterface)))
 				{
-					if (baseTypes.AllImplementThisInterface(@interface.FullName))
+					ITypeDefOrRef interfaceReference = potentialInterface.ToTypeDefOrRef();
+					group.Interface.AddInterfaceImplementation(interfaceReference);
+
+					if (interfaceReference is TypeDefinition interfaceType)
 					{
-						group.Interface.AddInterfaceImplementation(@interface);
+						existingInterfaces.UnionWith(interfaceType.GetAllInterfaces());
 					}
 				}
 			}
 		}
 
-		private static bool TryGetBaseTypeDefinitions(this IEnumerable<TypeDefinition> types, [NotNullWhen(true)] out List<TypeDefinition>? baseTypes)
+		private static HashSet<ITypeDefOrRef> GetAllInterfaces(this TypeDefinition type)
 		{
-			if (types.All(t => t.HasTypeDefinitionBaseType()))
+			HashSet<ITypeDefOrRef> result = new(SignatureComparer.Default);
+			HashSet<TypeDefinition> alreadyQueued = new()
 			{
-				baseTypes = types.Select(t => t.GetBaseTypeDefinition()).ToList();
-				return true;
+				type
+			};
+			Queue<TypeDefinition> queue = new()
+			{
+				type
+			};
+
+			while (queue.TryDequeue(out TypeDefinition? current))
+			{
+				foreach (InterfaceImplementation interfaceImplementation in current.Interfaces)
+				{
+					if (interfaceImplementation.Interface is not null)
+					{
+						result.Add(interfaceImplementation.Interface);
+						if (interfaceImplementation.Interface is TypeDefinition interfaceType && alreadyQueued.Add(interfaceType))
+						{
+							queue.Enqueue(interfaceType);
+						}
+					}
+				}
+				if (current.BaseType is TypeDefinition baseType && alreadyQueued.Add(baseType))
+				{
+					queue.Enqueue(baseType);
+				}
 			}
-			baseTypes = null;
-			return false;
+
+			return result;
 		}
 
-		private static TypeDefinition? TryGetBaseTypeDefinition(this TypeDefinition type)
+		private static void Add<T>(this Queue<T> queue, T item) => queue.Enqueue(item);
+
+		private static int GetInheritanceDepth(this TypeDefinition type)
 		{
-			return type.BaseType as TypeDefinition;
+			int depth = 0;
+			while (type.BaseType is TypeDefinition baseType)
+			{
+				depth++;
+				type = baseType;
+			}
+			return depth;
 		}
 
-		private static bool HasTypeDefinitionBaseType(this TypeDefinition type)
+		private sealed class GroupComparer : IComparer<ClassGroup>
 		{
-			return type.TryGetBaseTypeDefinition() != null;
-		}
+			public static GroupComparer Instance { get; } = new();
 
-		private static TypeDefinition GetBaseTypeDefinition(this TypeDefinition type)
-		{
-			return type.TryGetBaseTypeDefinition() ?? throw new Exception($"{type.Name} did not have a TypeDefinition base type");
-		}
+			int IComparer<ClassGroup>.Compare(ClassGroup? x, ClassGroup? y)
+			{
+				return x is null
+					? y is null ? 0 : -1
+					: y is null ? 1 : Compare(x, y);
+			}
 
-		private static bool AllImplementThisInterface(this IEnumerable<TypeDefinition> types, string interfaceFullName)
-		{
-			return types.All(t => t.Implements(interfaceFullName));
-		}
+			public static int Compare(ClassGroup x, ClassGroup y)
+			{
+				HashSet<TypeDefinition> xTypes = x.Types.ToHashSet();
+				HashSet<TypeDefinition> yTypes = y.Types.ToHashSet();
 
-		private static IEnumerable<ITypeDefOrRef> GetAllInterfaces(this TypeDefinition type)
-		{
-			return type.Interfaces.Select(t => t.Interface).Where(i => i != null).Select(j => j!);
+				bool xInheritsFromY = xTypes.Any(t => ContainsBaseType(yTypes, t));
+				bool yInheritsFromX = yTypes.Any(t => ContainsBaseType(xTypes, t));
+
+				return yInheritsFromX
+					? xInheritsFromY ? 0 : -1
+					: xInheritsFromY ? 1 : 0;
+
+				static bool ContainsBaseType(HashSet<TypeDefinition> set, TypeDefinition type)
+				{
+					while (type.BaseType is TypeDefinition baseType)
+					{
+						if (set.Contains(baseType))
+						{
+							return true;
+						}
+						type = baseType;
+					}
+					return false;
+				}
+			}
 		}
 	}
 }
