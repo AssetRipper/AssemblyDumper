1ee7ce42383e9e75b7fb7c9739be29f2a825b86c
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-20 13:37:00 -0500

    Fix bug in previous commit

--- generate.bat
+++ generate.bat
@@ -1,21 +1,21 @@
 dotnet build -c Debug
 
 
-cd ./AssetRipper.AssemblyDumper/bin/Debug/net7.0/
+cd ./AssetRipper.AssemblyDumper/bin/Debug/net8.0/
 
 AssetRipper.AssemblyDumper.exe --include-player-settings
 
 cd ../../../../
 
 
-cd ./AssetRipper.AssemblyDumper.Recompiler/bin/Debug/net7.0/
+cd ./AssetRipper.AssemblyDumper.Recompiler/bin/Debug/net8.0/
 
-AssetRipper.AssemblyDumper.Recompiler.exe ../../../../AssetRipper.AssemblyDumper/bin/Debug/net7.0/AssetRipper.SourceGenerated.dll ./Output/
+AssetRipper.AssemblyDumper.Recompiler.exe ../../../../AssetRipper.AssemblyDumper/bin/Debug/net8.0/AssetRipper.SourceGenerated.dll ./Output/
 
 cd ../../../../
 
 
-cd ./AssetRipper.AssemblyDumper.Recompiler/bin/Debug/net7.0/Output/
+cd ./AssetRipper.AssemblyDumper.Recompiler/bin/Debug/net8.0/Output/
 
 dotnet build -c Release
 
