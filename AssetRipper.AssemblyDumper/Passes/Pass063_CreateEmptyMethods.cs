acec70f7656d09100d0f75e79e5bd86ad4d73b11
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-01-05 21:31:42 -0500

    Remove code for serialization methods

--- AssetRipper.AssemblyDumper/Passes/Pass063_CreateEmptyMethods.cs
+++ AssetRipper.AssemblyDumper/Passes/Pass063_CreateEmptyMethods.cs
@@ -1,12 +1,10 @@
 ﻿using AssetRipper.AssemblyCreationTools.Methods;
 using AssetRipper.Assets;
 using AssetRipper.Assets.Export;
-using AssetRipper.Assets.IO.Serialization;
 using AssetRipper.Assets.IO.Writing;
 using AssetRipper.Assets.Metadata;
 using AssetRipper.IO.Endian;
 using AssetRipper.Yaml;
-using System.Text.Json.Nodes;
 
 namespace AssetRipper.AssemblyDumper.Passes
 {
@@ -23,11 +21,6 @@ namespace AssetRipper.AssemblyDumper.Passes
 			TypeSignature assetWriterRef = SharedState.Instance.Importer.ImportTypeSignature<AssetWriter>();
 			TypeSignature exportContainerInterfaceRef = SharedState.Instance.Importer.ImportTypeSignature<IExportContainer>();
 			TypeSignature yamlNodeRef = SharedState.Instance.Importer.ImportTypeSignature<YamlNode>();
-			TypeSignature jsonNodeRef = SharedState.Instance.Importer.ImportTypeSignature<JsonNode>();
-			TypeSignature serializerRef = SharedState.Instance.Importer.ImportTypeSignature<IUnityAssetSerializer>();
-			TypeSignature serializationOptionsRef = SharedState.Instance.Importer.ImportTypeSignature<SerializationOptions>();
-			TypeSignature deserializerRef = SharedState.Instance.Importer.ImportTypeSignature<IUnityAssetDeserializer>();
-			TypeSignature deserializationOptionsRef = SharedState.Instance.Importer.ImportTypeSignature<DeserializationOptions>();
 
 			foreach (TypeDefinition type in SharedState.Instance.AllNonInterfaceTypes)
 			{
@@ -48,17 +41,6 @@ namespace AssetRipper.AssemblyDumper.Passes
 
 				type.AddMethod(nameof(UnityAssetBase.ExportYamlEditor), OverrideMethodAttributes, yamlNodeRef)
 					.AddParameter(exportContainerInterfaceRef, "container");
-
-				//type.AddSerializeMethod(nameof(UnityAssetBase.SerializeReleaseFields), jsonNodeRef, serializerRef, serializationOptionsRef);
-
-				//type.AddSerializeMethod(nameof(UnityAssetBase.SerializeEditorFields), jsonNodeRef, serializerRef, serializationOptionsRef);
-
-				//type.AddSerializeMethod(nameof(UnityAssetBase.SerializeAllFields), jsonNodeRef, serializerRef, serializationOptionsRef);
-
-				//MethodDefinition deserializeMethod = type.AddMethod(nameof(UnityAssetBase.Deserialize), OverrideMethodAttributes, SharedState.Instance.Importer.Void);
-				//deserializeMethod.AddParameter(jsonNodeRef, "node");
-				//deserializeMethod.AddParameter(deserializerRef, "deserializer");
-				//deserializeMethod.AddParameter(deserializationOptionsRef, "options");
 
 				type.AddMethod(nameof(UnityAssetBase.Reset), OverrideMethodAttributes, SharedState.Instance.Importer.Void);
 			}
