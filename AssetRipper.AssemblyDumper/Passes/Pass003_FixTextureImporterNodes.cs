419d79b086c06e2295f6be699a59dd6313d4624d
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-01-14 00:55:17 -0500

    Inject a base class for textures with image data

--- AssetRipper.AssemblyDumper/Passes/Pass003_FixTextureImporterNodes.cs
+++ AssetRipper.AssemblyDumper/Passes/Pass003_FixTextureImporterNodes.cs
@@ -1,4 +1,6 @@
-﻿namespace AssetRipper.AssemblyDumper.Passes
+﻿using AssetRipper.AssemblyDumper.Utils;
+
+namespace AssetRipper.AssemblyDumper.Passes
 {
 	/// <summary>
 	/// This pass deliberately modifies the type tree for TextureImporter to reflect its yaml output.
@@ -21,6 +23,11 @@
 
 		private const int TextureImporterTypeId = 1006;
 
+		private const int TextureId = 27;
+
+		private const string ImageTextureName = "ImageTexture";
+		private const int ImageTextureId = 189;//This is currently unused and is close to Texture2DArray (187) and CubemapArray (188)
+
 		public static void DoPass()
 		{
 			foreach (UniversalClass? universalClass in SharedState.Instance.ClassInformation[TextureImporterTypeId].Values)
@@ -31,6 +38,9 @@
 				}
 			}
 
+			//ImageTexture is a fabricated class that represents a texture with pixel data.
+			CreateArtificialDerivedClass(ImageTextureName, ImageTextureId, TextureId);
+
 			//VideoClipImporter
 			ChangeBaseClass(1127, "AssetImporter");
 
@@ -39,6 +49,18 @@
 
 			//EditorBuildSettings
 			ChangeBaseClass(1045, "GlobalGameManager");
+
+			//Texture2D
+			ChangeBaseClass(28, ImageTextureName);
+
+			//Texture3D
+			ChangeBaseClass(117, ImageTextureName);
+
+			//Texture2DArray
+			ChangeBaseClass(187, ImageTextureName);
+
+			//CubemapArray
+			ChangeBaseClass(188, ImageTextureName);
 		}
 
 		private static void ChangeBaseClass(int classId, string baseClass)
@@ -50,6 +72,40 @@
 					universalClass.BaseString = baseClass;
 				}
 			}
+		}
+
+		private static void CreateArtificialDerivedClass(string className, int classId, int baseClassId)
+		{
+			VersionedList<UniversalClass> list = new();
+			foreach ((UnityVersion version, UniversalClass? rendererClass) in SharedState.Instance.ClassInformation[baseClassId])
+			{
+				if (rendererClass is null)
+				{
+					list.Add(version, null);
+				}
+				else
+				{
+					UniversalClass duplicate = rendererClass.DeepClone();
+
+					duplicate.Name = className;
+					duplicate.OriginalName = className;
+					duplicate.TypeID = classId;
+					duplicate.OriginalTypeID = classId;
+					duplicate.IsAbstract = true;
+					duplicate.BaseString = rendererClass.Name;
+					if (duplicate.ReleaseRootNode is not null)
+					{
+						duplicate.ReleaseRootNode.TypeName = className;
+					}
+					if (duplicate.EditorRootNode is not null)
+					{
+						duplicate.EditorRootNode.TypeName = className;
+					}
+
+					list.Add(version, duplicate);
+				}
+			}
+			SharedState.Instance.ClassInformation.Add(classId, list);
 		}
 
 		private static void DoPassOnClass(UniversalClass universalClass)
