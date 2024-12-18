8d0e8b6bc7f7bc447f4619e10a8d83cf309221b6
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-12-17 22:32:18 -0800

    OffsetPtr ToString override

--- AssetRipper.AssemblyDumper/Passes/Pass203_OffsetPtrImplicitConversions.cs
+++ AssetRipper.AssemblyDumper/Passes/Pass203_OffsetPtrImplicitConversions.cs
@@ -2,6 +2,9 @@
 
 namespace AssetRipper.AssemblyDumper.Passes
 {
+	/// <summary>
+	/// Adds implicit conversion and ToString override to OffsetPtr types.
+	/// </summary>
 	public static class Pass203_OffsetPtrImplicitConversions
 	{
 		const MethodAttributes ConversionAttributes = MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.SpecialName | MethodAttributes.HideBySig;
@@ -15,6 +18,7 @@ namespace AssetRipper.AssemblyDumper.Passes
 					foreach (TypeDefinition type in group.Types)
 					{
 						type.AddImplicitConversion();
+						type.AddToStringOverride();
 					}
 				}
 			}
@@ -31,6 +35,22 @@ namespace AssetRipper.AssemblyDumper.Passes
 
 			processor.Add(CilOpCodes.Ldarg_0);
 			processor.Add(CilOpCodes.Ldfld, field);
+			processor.Add(CilOpCodes.Ret);
+		}
+
+		private static void AddToStringOverride(this TypeDefinition type)
+		{
+			const MethodAttributes attributes = MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.ReuseSlot | MethodAttributes.HideBySig;
+
+			CorLibTypeSignature stringType = type.Module!.CorLibTypeFactory.String;
+			ITypeDefOrRef objectType = type.Module.CorLibTypeFactory.Object.ToTypeDefOrRef();
+
+			MethodDefinition toStringMethod = type.AddMethod(nameof(ToString), attributes, stringType);
+
+			CilInstructionCollection processor = toStringMethod.CilMethodBody!.Instructions;
+			processor.Add(CilOpCodes.Ldarg_0);
+			processor.Add(CilOpCodes.Ldfld, type.GetField());
+			processor.Add(CilOpCodes.Callvirt, new MemberReference(objectType, nameof(ToString), MethodSignature.CreateInstance(stringType)));
 			processor.Add(CilOpCodes.Ret);
 		}
 
